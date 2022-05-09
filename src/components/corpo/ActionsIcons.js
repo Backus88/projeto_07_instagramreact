5f98b17b1650c37dd499ab0c6f8f5cff0a94697f
Luis <lf.mc.almeida@poli.ufrj.br>
2022-05-09 14:44:24 -0300

    completo

--- src/components/corpo/ActionsIcons.js
+++ src/components/corpo/ActionsIcons.js
@@ -1,10 +1,27 @@
 import Ion from "../Ion";
+import React from "react";
 
 export default function ActionsIcons (){
+   
     const itens = ["heart-outline", "chatbubble-outline", "paper-plane-outline"];
+    const [heart, setHeart]= React.useState("heart-outline");
+    const chat = "chatbubble-outline";
+    const paper = "paper-plane-outline";
+
+    function curtida (){
+        console.log("aki")
+        if (heart === "heart-outline"){
+            setHeart("heart")
+        }else{
+            setHeart("heart-outline");
+        }
+    }
+
     return(
-        <div>
-             {itens.map(item => <Ion  nome = {item}   />)}
+        <div >
+            <ion-icon name={heart} role = "button" onClick = {curtida} ></ion-icon> 
+             <Ion nome = {chat} />
+             <Ion nome = {paper} />
         </div>
     );
 
